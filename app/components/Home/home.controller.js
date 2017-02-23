cb2cfe25bb8b3ec82b3771df7c22b5333691b151
Alex Mac <alexmac3@uw.edu>
2017-02-23 08:38:56 -0800

    implement basic game functionality

--- app/components/Home/home.controller.js
+++ app/components/Home/home.controller.js
@@ -1,3 +1,9 @@
 app.controller('HomeCtrl', ['$scope', function($scope) {
   $scope.status = ['', '', '', '', '', '', '', '', ''];
+
+  let currentSymbol = 'O';
+  $scope.check = function(index) {
+    $scope.status[index] = currentSymbol;
+    currentSymbol = currentSymbol === 'O' ? 'X' : 'O';
+  }
 }]);
