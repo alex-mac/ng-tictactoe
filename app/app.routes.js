c8a0ad9687f8ff4d124d51324416436c8bde93da
Alex Mac <alexmac3@uw.edu>
2017-02-22 16:40:48 -0800

    initial commit

---
+++ app/app.routes.js
@@ -0,0 +1,12 @@
+app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+  $routeProvider.when('/', {
+    templateUrl: 'app/components/Home/home.view.html',
+    controller: 'HomeCtrl'
+  })
+  .otherwise({
+    templateUrl: 'app/components/404/404.view.html',
+    controller: '404Ctrl'
+  });
+
+  $locationProvider.html5Mode(false).hashPrefix('!');
+}]);
